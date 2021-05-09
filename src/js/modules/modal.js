8a29ffefdde431f17b55fb1e980cdc47504c8721
Ksenia Balashova <utyatka@gmail.com>
2021-05-09 19:05:23 +0200

    add popup

--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -1,10 +1,5 @@
-// var MicroModal = require('micromodal');
-// MicroModal.show('deleteModal');
-// MicroModal.close('deleteModal');
-
-// 'use strict';
-
-// (function () {
-//   MicroModal.show('deleteModal');
-//   MicroModal.close('deleteModal');
-// })();
+$(document).ready(function(){
+  $(".order__cancel").click(function(){
+    $("#deleteModal").modal();
+  });
+});
