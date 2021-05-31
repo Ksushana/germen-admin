54071d6afbf0e3743e82e51e086a1421f0f6b735
Ksenia Balashova <utyatka@gmail.com>
2021-05-31 15:19:05 +0200

    js button

--- src/js/modules/form.js
+++ src/js/modules/form.js
@@ -10,6 +10,20 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
+
+  $('.courier-order-info__btn .courier__btn').click(function(){
+    const text = $(this).text();
+    const routeText = 'В пути / доставлен';
+    const deliveredText = 'Доставлен';
+    $(this).html(routeText);
+    if (text == routeText){
+      $(this).html(deliveredText);
+      $(this).css( "pointer-events", "none" );
+  };
+    });
+});
+
+$(document).ready(function(){
   $('.courier-contacts__btn .courier__btn').click(function(){
     $('.courier-tabs__link--second').addClass( "js-tabs__title-active" );
     $('.courier-tabs__link--first').removeClass( "js-tabs__title-active" );
